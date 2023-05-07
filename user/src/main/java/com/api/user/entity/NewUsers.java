cf66e1e5691118a67e38c5ea14960cedf532e2a5
Manoranjan <sahoomanoranjan626@gmail.com>
2023-05-07 23:54:45 +0530

    Added swagger configuration to users module

--- user/src/main/java/com/api/user/entity/NewUsers.java
+++ user/src/main/java/com/api/user/entity/NewUsers.java
@@ -1,8 +1,6 @@
 package com.api.user.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -10,6 +8,8 @@ import java.util.Date;
 public class NewUsers {
 
     @Column(name = "ID")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "USER_ID")
